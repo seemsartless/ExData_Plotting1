6f01ed94368e395366e2cd174f732c253273e908
seemsartless <david.s.toronto@gmail.com>
2015-11-07 11:52:34 -0500

    Initial version, just comments so far

---
+++ plot1.R
@@ -0,0 +1,13 @@
+#
+# plot1.R - david Sky - david.s.toronto@gmail.com
+#
+#    Exploratory Data Analysis course - Excercise 1
+#
+#    Read in the "Individual household electric power consumption Data Set" and create a
+#    bar chart for the dates between 2007-02-01 and 2007-02-02
+#
+
+#    Steps:
+#      1. Read in the data file, making sure the columns we need are in the required format
+#      2. Set the column names to something more useful
+#      3. Creat the graph using the basic graphing functions
